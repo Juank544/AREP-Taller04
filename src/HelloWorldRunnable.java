df213b48d693acce00387f794ec465050f0176cb
Juank544 <camilo.gv@live.com>
2022-03-09 15:47:27 -0500

    sincronización de hilos

--- src/HelloWorldRunnable.java
+++ src/HelloWorldRunnable.java
@@ -3,12 +3,21 @@ public class HelloWorldRunnable implements Runnable{
     @Override
     public void run() {
         System.out.println("Hello from runnable object:" + Thread.currentThread());
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args){
         Thread t = new Thread(new HelloWorldRunnable());
         t.start();
-
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("Hello from main; " + Thread.currentThread());
     }
 }
