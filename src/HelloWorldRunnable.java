20e86da6e7c78b5a142cb6d2a734f07804edd870
Juank544 <camilo.gv@live.com>
2022-03-09 22:49:21 -0500

    pool de hilos con executor

--- src/HelloWorldRunnable.java
+++ src/HelloWorldRunnable.java
@@ -1,23 +1,34 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class HelloWorldRunnable implements Runnable{
 
     @Override
     public void run() {
         System.out.println("Hello from runnable object:" + Thread.currentThread());
         try {
-            Thread.sleep(4000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args){
-        Thread t = new Thread(new HelloWorldRunnable());
-        t.start();
-        try {
-            t.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+        int n = 100;
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+        while (n>0){
+            pool.submit(new HelloWorldRunnable());
+            n = n-1;
         }
+
+//        Thread t = new Thread(new HelloWorldRunnable());
+//        t.start();
+//        try {
+//            t.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         System.out.println("Hello from main; " + Thread.currentThread());
     }
 }
